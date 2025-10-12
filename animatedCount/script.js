dac816818985751893ddf059949d4365f3e064ea
JOYDIP MANNA <JOYDIPMANNA2002@GMAIL.COM>
2025-10-12 23:28:09 +0530

    12-10-2025

---
+++ animatedCount/script.js
@@ -0,0 +1,34 @@
+const counters = document.querySelectorAll(".counter");
+const speed = 120; // Lower = faster
+
+counters.forEach((counter) => {
+    const updateCount = () => {
+        const target = +counter.getAttribute("data-target");
+        const count = +counter.innerText;
+        const increment = target / speed;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 20);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
